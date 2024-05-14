c690a981e08473e293361f607cf1587026165da6
U220717040 <alikorayurun@posta.mu.edu.tr>
2024-05-15 01:43:36 +0300

    Classes' fields and methods initialized.

--- hospitalmanagement/Patient.java
+++ hospitalmanagement/Patient.java
@@ -1,4 +1,8 @@
 package hospitalmanagement;
 
 public class Patient extends Person{
+    public Patient(String citizenNumber, String name, String surname,
+                   String email, String phone, String gender, int age) {
+        super(citizenNumber, name, surname, email, phone, gender, age);
+    }
 }
