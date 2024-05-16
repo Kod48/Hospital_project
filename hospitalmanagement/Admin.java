0320a13f913d8e796a5bbd3fac5569cf1e1a8547
Serdar1048 <serdardedebas1@gmail.com>
2024-05-16 15:28:00 +0300

    Admin's menu is added.

--- hospitalmanagement/Admin.java
+++ hospitalmanagement/Admin.java
@@ -1,18 +1,44 @@
 package hospitalmanagement;
 
+import operations.*;
+
+import java.util.Scanner;
+
 public class Admin extends Staff{
     public Admin(/*IOoperations[] operations,*/String citizenNumber, String name, String surname,
                  String email, String phone, String gender, int age, double salary) {
         super(citizenNumber, name, surname, email, phone, gender, age, salary);
+        this.operations = new IOOperations[]{
+                new Exit(),
+                new Hire(),
+                new Fire(),
+                new UpdateSalary(),
+                new Search()
+        };
     }
     public Admin(String citizenNumber, String name,String surname,String phone){
         super(citizenNumber, name, surname, phone);
+        this.operations = new IOOperations[]{
+                new Exit(),
+                new Hire(),
+                new Fire(),
+                new UpdateSalary(),
+                new Search()
+        };
     }
     @Override
 
     public void menu(Database database) {
+        System.out.println("0. Exit");
+        System.out.println("1. Hire");
+        System.out.println("2. Fire");
+        System.out.println("3. Update Salary");
+        System.out.println("4. Search");
 
-        System.out.println("Adminn menu");
+        Scanner scanner = new Scanner(System.in);
+        int choice = scanner.nextInt();
+        this.operations[choice].oper(database);
+        scanner.close();;
     }
 
     @Override
